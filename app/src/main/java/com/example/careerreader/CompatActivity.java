788e43136c9663d1d0ab1b75740a8bbf95110344
Kameron Bryant <bryantkp@vcu.edu>
2019-10-11 14:13:42 -0400

    Merge branch 'master' of /Users/kameronbryant/Documents/GitHub/fall-19-project-appmonsters with conflicts.

---
+++ app/src/main/java/com/example/careerreader/CompatActivity.java
@@ -0,0 +1,14 @@
+package com.example.careerreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compat);
+    }
+}
