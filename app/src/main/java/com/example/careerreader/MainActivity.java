13f56a8d8ccceeb27d0f015ab82ab7c6ceeb4c05
EswarVeerapaneni <46966138+EswarVeerapaneni@users.noreply.github.com>
2019-10-11 13:41:29 -0400

    updated the first activity and almost finished the second Activity
    Constrains are a bit work in progress, will work on it once u guys tell me the number of buttons u need for each division

--- app/src/main/java/com/example/careerreader/MainActivity.java
+++ app/src/main/java/com/example/careerreader/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.careerreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button findC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findC = findViewById(R.id.FindCollegeButton);
+        findC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+//    public void goBack(View view)
+//    {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.putExtra("message", password);
+//        setResult(RESULT_OK,intent);
+//        finish();
+//        // startActivity(intent);
+//
+//    }
+
+
 }
