1c4d661bedbefc53f96ccb7cb1371ade05786f07
mccullochme <mccullochme@users.noreply.github.com>
2019-10-14 15:38:10 -0400

    Back Button Functionality
    added the functionality to the back button

--- app/src/main/java/com/example/careerreader/Main3Activity.java
+++ app/src/main/java/com/example/careerreader/Main3Activity.java
@@ -1,6 +1,9 @@
 package com.example.careerreader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 import android.os.Bundle;
 
@@ -10,5 +13,14 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Button backButton = findViewById(R.id.backButton);
     }
+
+    public void goBack(View view)
+    {
+        Intent intent = new Intent(Main3Activity.this , MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
