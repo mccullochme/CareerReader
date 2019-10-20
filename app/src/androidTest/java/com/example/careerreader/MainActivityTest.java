f1c4ce29edc87d4ae5171babeeda87610215cf0a
Jorge <jzaquitzal@gmail.com>
2019-10-20 17:36:15 -0400

    Finished 9 espresso 3 per user story

--- app/src/androidTest/java/com/example/careerreader/MainActivityTest.java
+++ app/src/androidTest/java/com/example/careerreader/MainActivityTest.java
@@ -45,6 +45,7 @@ public class MainActivityTest {
         */
         onView(withId(R.id.FindCollegeButton)).perform(click());
         onView(withId(R.id.mathBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
     }
     @Test
     public void testSelectAllMajors(){
@@ -59,43 +60,77 @@ public class MainActivityTest {
         onView(withId(R.id.historyBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
         onView(withId(R.id.englishBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
         onView(withId(R.id.artBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
     }
     // User Story 2 Select College
+    @Test
+    public void testFindCollegeButton(){
+        /*
+        Select College by price
+        Scenario 1
+        * This test, tests the FindCollegeButton that will then
+        take you to the next page where you are prompt to select a price range
+        */
+        onView(withId(R.id.FindCollegeButton)).perform(click());
+    }
     @Test
     public void testPriceRange1(){
         /*
         Select College By Price
-        Scenario 1
+        Scenario 2
         *
-        This tests the ability to select a college in the price range between $0-$20,00
+        This tests the ability to select a college in the price range between $20,00-30,00
         */
         onView(withId(R.id.FindCollegeButton)).perform(click());
-        onView(withId(R.id.zeroToFive)).check(matches(isNotChecked())).perform(scrollTo(), click());
-        onView(withId(R.id.backMessage)).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.fiveToTen)).perform(click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
     }
     @Test
     public void testPriceRange2(){
         /*
-        Select College
-        Scenario 2
-        * This test, tests the ability to select a major by clicking on a checkbox
+        Select College By Price
+        Scenario 3
+        This tests the ability to select a college in the price range between $0-$20,00 and $40,00
         */
         onView(withId(R.id.FindCollegeButton)).perform(click());
-        onView(withId(R.id.mathBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
+        onView(withId(R.id.zeroToFive)).perform(click());
+        onView(withId(R.id.twentyPlus)).perform(click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
+    }
+    // User story 3 Find private or public colleges
+    @Test
+    public void testSelectingPrivateSchoolBox(){
+        /*
+        Find public or private colleges
+        Scenario 1
+        This test, tests the ability to select only private schools
+        */
+        onView(withId(R.id.FindCollegeButton)).perform(click());
+        onView(withId(R.id.privateBox)).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
     }
     @Test
-    public void testPriceRange3(){
+    public void testSelectingPublicSchoolBox(){
         /*
-        Select College
-        Scenario 3
-        * This test, tests the ability to select a major by clicking on a checkbox
+        Find public or private colleges
+        Scenario 2
+        This test, tests the ability to select only public schools
         */
         onView(withId(R.id.FindCollegeButton)).perform(click());
-        onView(withId(R.id.mathBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
-        onView(withId(R.id.scienceBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
-        onView(withId(R.id.historyBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
-        onView(withId(R.id.englishBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
-        onView(withId(R.id.artBox)).check(matches(isNotChecked())).perform(scrollTo(), click());
+        onView(withId(R.id.publicBox)).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
+    }
+    @Test
+    public void testSelectingPublicAndPrivate(){
+        /*
+        Find public or private colleges
+        Scenario 3
+        This test, tests the ability to select both public and private schools
+        */
+        onView(withId(R.id.FindCollegeButton)).perform(click());
+        onView(withId(R.id.publicBox)).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.privateBox)).perform(ViewActions.scrollTo()).perform(click());
+        onView(withId(R.id.button8)).perform(ViewActions.scrollTo()).perform(click());
     }
 
     @Before
