d37696d0f653d709ce239cd4b53b86065db49cbb
Jorge <jzaquitzal@gmail.com>
2019-10-21 15:08:19 -0400

    cleaned up espresso code to remove unused imports

--- app/src/main/java/com/example/careerreader/SelectionScreen.java
+++ app/src/main/java/com/example/careerreader/SelectionScreen.java
@@ -21,7 +21,7 @@ public class SelectionScreen extends AppCompatActivity {
     College GMU = new College("Science", 19000, "Suburban", 23179, "Public", "George Mason University", "ehlop");
     College VTECH = new College("Math", 19000, "Rural", 25175, "Public", "Virginia Polytechnic University", "ehlop");
     College JMU = new College("History", 20000, "Rural", 19262, "Public", "James Madison University", "ehlop");
-    College HAMPTON = new College("Sience", 34000, "Urban", 3793, "Private", "Hampton University", "ehlop");
+    College HAMPTON = new College("Science", 34000, "Urban", 3793, "Private", "Hampton University", "ehlop");
     College LU = new College("Math", 25000, "Suburban", 46680, "Private", "Liberty University", "ehlop");
     College URVA = new College("Art", 25000, "Suburban", 3229, "Private", "University of Richmond", "ehlop");
     College RADFORD = new College("History", 17000, "Rural", 8426, "Public", "Radford University", "ehlop");
@@ -61,10 +61,10 @@ public class SelectionScreen extends AppCompatActivity {
          history = findViewById(R.id.businessBox);
          english = findViewById(R.id.healthBox);
          art = findViewById(R.id.artBox);
-         zeroToFive = findViewById(R.id.zeroToFive);
-         fiveToTen = findViewById(R.id.fiveToTen);
-         tenToTwenty = findViewById(R.id.tenToTwenty);
-         twentyPlus = findViewById(R.id.twentyPlus);
+         zeroToFive = findViewById(R.id.zeroToTwenty);
+         fiveToTen = findViewById(R.id.twentyToThirty);
+         tenToTwenty = findViewById(R.id.thirtyToForty);
+         twentyPlus = findViewById(R.id.fortyPlus);
          rural = findViewById(R.id.ruralBox);
          subUrban = findViewById(R.id.suburbanBox);
          urban = findViewById(R.id.urbanBox);
