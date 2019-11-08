4cdbaea54e0a593d679b02f625cf742b13f7e42d
Kameron Bryant <bryantkp@vcu.edu>
2019-11-08 16:23:47 -0500

    New Major Selection Screen with radio groups and Radio Buttons

---
+++ app/src/main/java/com/example/careerreader/MajorSelectionScreen.java
@@ -0,0 +1,15 @@
+package com.example.careerreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MajorSelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_major_selection_screen);
+    }
+
+}
